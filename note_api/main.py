6412ff8cd71cd82f80ba5c7edb458cda0ed016b6
Mohamad Omran Scharaf <128322385+mscharaf@users.noreply.github.com>
2025-01-12 23:19:19 +0000

    Fix OpenTelemetry imports and pass all tests

--- note_api/main.py
+++ note_api/main.py
@@ -12,7 +12,7 @@ from opentelemetry import trace
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.trace import SpanKind
@@ -27,10 +27,10 @@ tracer_provider = TracerProvider(resource=resource)
 trace.set_tracer_provider(tracer_provider)
 
 # Cloud Trace Exporter einrichten
-cloud_trace_exporter = CloudTraceSpanExporter(credentials=credentials)
+cloud_trace_exporter = CloudTraceSpanExporter()
 
 # BatchSpanProcessor hinzufügen
-span_processor = SimpleExportSpanProcessor(cloud_trace_exporter)
+span_processor = SimpleSpanProcessor(cloud_trace_exporter)
 tracer_provider.add_span_processor(span_processor)
 
 
